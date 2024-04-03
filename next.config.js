b1b047a7a126769710deeb0589e8759e05473dee
XkotX <bartekbrotek81@gmail.com>
2024-04-03 22:36:45 +0200

    ustawienie cors

--- next.config.js
+++ next.config.js
@@ -4,5 +4,18 @@ const nextConfig = {
         domains: ['cdn.pixabay.com', 'media.istockphoto.com', 'gorka.rawa-kopernik.pl', 'drive.google.com', "google.com", "istockphoto.com"],
     },
 }
-
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+                ],
+            },
+        ];
+    },
+};
 module.exports = nextConfig
