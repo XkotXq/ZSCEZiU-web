8a78503b25fc78713eb832b8cb0d72ebfd4695eb
XkotX <bartekbrotek81@gmail.com>
2024-04-14 19:52:50 +0200

    naprawa authOptions 4

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,20 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "../../../../../utils/connect";
+import User from "../../../../../models/userModel";
+import bcrypt from "bcryptjs"
+
+async function login(credentials) {
+    try {
+        connectDB();
+        const user = await User.findOne({username:credentials.username});
+        if(!user) throw new Error("Błędne dane uwierzytelniające.");
+        const isCorret = await bcrypt.compare(credentials.password, user.password);
+        if(!isCorret) throw new Error("Błędne dane uwierzytelniające.");
+        return user
+    } catch (error) {
+        throw new Error("Coś poszło nie tak.")
+    }
+}
 
 export const authOptions = {
     pages: {
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,7 @@
-import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth/next";
-import { connectDB } from "../../../../../utils/connect";
-import User from "../../../../../models/userModel";
-import bcrypt from "bcryptjs"
 import { authOptions } from "./options"
 
-async function login(credentials) {
-    try {
-        connectDB();
-        const user = await User.findOne({username:credentials.username});
-        if(!user) throw new Error("Błędne dane uwierzytelniające.");
-        const isCorret = await bcrypt.compare(credentials.password, user.password);
-        if(!isCorret) throw new Error("Błędne dane uwierzytelniające.");
-        return user
-    } catch (error) {
-        throw new Error("Coś poszło nie tak.")
-    }
-}
+
 
 
 const handler = NextAuth(authOptions);
